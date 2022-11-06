5853827b2c0eefc2c4ff1907784f6ebfe873f8f6
chenbaizhuang <1989088585@qq.com>
2022-11-06 11:32:25 +0800

    路由组件的搭建

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <template>
-  <div id="app">
+  <div>
     <Header></Header>
-    dss
+    <!-- 路由组件出口的地方 -->
+    <router-view></router-view>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
 //引入
-import Header from "./components/Header/index.vue";
-import Footer from './components/Footer/index.vue'
+import  Header from "./components/Header";
+import Footer from './components/Footer'
 export default {
   name: "app",
   components: {
     Header,
     Footer
-  },
+  }
 };
 </script>
 

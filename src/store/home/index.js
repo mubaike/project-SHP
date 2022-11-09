d031eedf34a676ecf27e193c5f50f8ceda3e2fbf
chenbaizhuang <1989088585@qq.com>
2022-11-09 22:13:14 +0800

    三级联动组件获取数据与显示

--- src/store/home/index.js
+++ src/store/home/index.js
@@ -1,7 +1,23 @@
+import { reqCategoryList } from '@/api'
 //home模块的小仓库
-const state = {};
-const mutations = {};
-const actions = {};
+const state = {
+    //state中数据默认初始值，服务器返回对象、服务器返回数组。【根据接口返回值初始化】
+    catagoryList: []
+};
+const mutations = {
+    CATGORYLIST(state, catagoryList) {
+        state.catagoryList = catagoryList;
+    }
+};
+const actions = {
+    //通过API里面的接口函数调用，向服务器发请求，获取服务器的数据
+    async catagoryList({commit}){
+        let result = await reqCategoryList();
+        if(result.code==200){
+            commit("CATGORYLIST",result.data);
+        }
+    }
+};
 const getters = {};
 
 export default {
